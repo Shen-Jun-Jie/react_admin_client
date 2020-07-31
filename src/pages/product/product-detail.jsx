6de73bf4c7456f90cac8f1a0b5ca306838dbc0a5
沈俊杰 <shen@shenjunjiedeMacBook-Pro.local>
2020-07-31 23:27:37 +0800

    0731 11:30

--- src/pages/product/product-detail.jsx
+++ src/pages/product/product-detail.jsx
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
+import {
+    Card,
+    List
+} from 'antd'
 
-class Detail extends Component {
+import {ArrowLeftOutlined} from '@ant-design/icons'
+
+const {Item} = List
+
+class ProductDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
     render() { 
+
+        const title = (
+            <span>
+                <ArrowLeftOutlined />
+                <span>商品详情</span>
+            </span>
+        )
         return ( 
-            <div>
-                Detail
-            </div>
+            <Card title={title} className="product-detail">
+                <List>
+                    <Item>
+                        <span className="left">商品名称：</span>
+                        <span>Tinkpad 翼480</span>
+                    </Item>
+                    <Item>
+                        <span className="left">商品描述：</span>
+                        <span>Tinkpad 翼480</span>
+                    </Item>
+                    <Item>
+                        <span className="left">商品名称：</span>
+                        <span>Tinkpad 翼480</span>
+                    </Item>
+                    <Item>
+                        <span className="left">商品名称：</span>
+                        <span>Tinkpad 翼480</span>
+                    </Item>
+                    <Item>
+                        <span className="left">商品名称：</span>
+                        <span>Tinkpad 翼480</span>
+                    </Item>
+                </List>
+            </Card>
          );
     }
 }
  
-export default Detail;
+export default ProductDetail;
